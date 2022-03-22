cb364f2a7e9428a30c70fe88898ec96acb86eff8
Anton Mishkevich <mian19@protonmail.com>
2022-03-22 21:45:30 +0300

    init: start hwk18 project

---
+++ hwk18_PhotoLibrary/hwk18_PhotoLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hwk18_PhotoLibrary
+//
+//  Created by Kyzu on 22.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
